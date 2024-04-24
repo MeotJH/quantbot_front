63caaebfe7a1b84b05f9ff8cb4655ae75e4c2db2
김진한 <kimjh0417@gsitm.com>
2024-04-24 17:16:42 +0900

    ADD card for news view in main

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
@@ -58,6 +61,45 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
+            Card(
+              color: const Color(0xff284363),
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      width: 300,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                      ),
+                      child: Image.asset(
+                        'assets/icon.png',
+                      ),
+                    ),
+                    const Text(
+                      '1인가구는 원룸만?…임대주택 면적논란에 국토부 "원점재검토"(종합)',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text('button'),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
