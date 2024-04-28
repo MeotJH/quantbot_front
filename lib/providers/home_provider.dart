b56b4f0f765d9419d31303db7fc419b07b287bde
Meotjh <marionette934@gmail.com>
2024-04-28 12:16:54 +0900

    fix cors origin for home_page cors error

--- lib/providers/home_provider.dart
+++ lib/providers/home_provider.dart
@@ -2,9 +2,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quant_bot_front/models/home_model.dart';
 import 'package:quant_bot_front/providers/dio_providers';
 
-final AutoDisposeFutureProvider<List<HomeModel>> homepageProvider = FutureProvider.autoDispose((ref) async {
+final AutoDisposeFutureProvider<List<HomeModel>> homepageProvider =
+    FutureProvider.autoDispose((ref) async {
   final dio = ref.read(dioProvider);
-  final response = await dio.get('/api/v1/news/6'); //await dio.get('/api/v1/news/6');
+  final response =
+      await dio.get('/api/v1/news/6'); //await dio.get('/api/v1/news/6');
 
   if (response.statusCode != 200) {
     return [];
